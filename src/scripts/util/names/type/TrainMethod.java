54ae2870ad9415630b63d51549a9d9fc8ac810a8
orange451 <drew.hamilton@mac.com>
2020-04-22 19:54:42 -0400

    Added TrainMethod#getLowestSkill

--- src/scripts/util/names/type/TrainMethod.java
+++ src/scripts/util/names/type/TrainMethod.java
@@ -1,14 +1,52 @@
 package scripts.util.names.type;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.tribot.api2007.Skills.SKILLS;
+
 import scripts.util.misc.AntiBan;
 
 public enum TrainMethod {
-	ATTACK_XP,
-	STRENGTH_XP,
-	DEFENSE_XP;
+	ATTACK_XP(SKILLS.ATTACK),
+	STRENGTH_XP(SKILLS.STRENGTH),
+	DEFENSE_XP(SKILLS.DEFENCE);
+	
+	private SKILLS skill;
+	
+	TrainMethod(SKILLS skill) {
+		this.skill = skill;
+	}
+	
+	public SKILLS getSkill() {
+		return this.skill;
+	}
 
+	/**
+	 * Return a random TrainMethod
+	 * @return
+	 */
 	public static TrainMethod random() {
 		TrainMethod[] values = values();
 		return values[AntiBan.random(values.length)];
+	}
+	
+	/**
+	 * Return the train method that trains your lowest skill. (Picks at random if there's a tie)
+	 * @return
+	 */
+	public static TrainMethod getLowestSkill() {
+		List<TrainMethod> temp = new ArrayList<>();
+		int min = Integer.MAX_VALUE;
+		
+		for (TrainMethod method : values()) {
+			SKILLS skill = method.getSkill();
+			if ( skill.getActualLevel() <= min ) {
+				min = skill.getActualLevel();
+				temp.add(method);
+			}
+		}
+		
+		return temp.get(AntiBan.random(temp.size()));
 	}
 }
