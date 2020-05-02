10dc49ac1a5cbddaf8480d5ffe9f555375f35018
orange451 <drew.hamilton@mac.com>
2020-05-02 19:08:47 -0400

    Name Formatter now more closely follow runescape text formatting

--- scripts/util/misc/NameFormatter.java
+++ scripts/util/misc/NameFormatter.java
@@ -3,7 +3,7 @@ package scripts.util.misc;
 public class NameFormatter {
 	/**
 	 * Returns a properly formatted string. <br>
-	 * Example: IRON_HELMET -> Iron Helmet
+	 * Example: IRON_HELMET -> Iron helmet
 	 * @param input
 	 * @return
 	 */
@@ -14,7 +14,13 @@ public class NameFormatter {
 		StringBuffer sb = new StringBuffer();
 
 		for (int i = 0; i < arr.length; i++) {
-			sb.append(Character.toUpperCase(arr[i].charAt(0))).append(arr[i].substring(1)).append(" ");
+			if ( i == 0 )
+				sb.append(Character.toUpperCase(arr[i].charAt(0))).append(arr[i].substring(1));
+			else
+				sb.append(arr[i]);
+			
+			if ( i < arr.length - 1 )
+				sb.append(" ");
 		}
 		return sb.toString().trim();
 	}
